823a60857bc6297c4a7c9259d915a528572c38b4
streetdoggs <bobangaraymond@gmail.com>
2025-05-30 11:19:30 +0200

    lexas
    portfolio web

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+
+    function quitterSite() {
+      // Tente de fermer la fenêtre (ne fonctionne que si la page a été ouverte par script)
+      window.close();
+
+      // Si l'onglet ne se ferme pas, redirige vers un autre site
+      setTimeout(() => {
+        window.location.href = "https://www.google.fr";
+      }, 500);
+    }
+   function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
